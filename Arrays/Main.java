f0e9fb2a9e37c4cd4ba80d0ae251808a1bbb926b
Swathi <swathignanavel1355@gmail.com>
2026-05-06 11:36:24 +0530

    Main.java file added

---
+++ Arrays/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ArrayInput {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("the elements are");
+            arr[i]=scan.nextInt();
+        }
+         for(int i=0;i<n;i++){
+              System.out.print("the output elements are");
+             
+        System.out.println(arr[i]);
+    }
+    }
+}
